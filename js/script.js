ba57e68bd3e5d10cb4e842997724eacf89f3b012
AlexandruGeorgianBahnaru <130040776+AlexandruGeorgianBahnaru@users.noreply.github.com>
2024-03-21 11:43:13 +0200

    Laborator5.1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 
+const x = document.getElementById("locatie");
 window.onload = function dateTimeFunction()
 {
     var date = new Date();
     var dateTime = document.getElementById("dateTime");
     dateTime.innerHTML = date.toLocaleString();
 
-    var URL1 = new URL();
     var url = document.getElementById("url");
-    url.innerHTML = URL1.toString();
+    url.innerHTML = document.location;
+
+    var locatie = document.getElementById("locatie");
+    navigator.geolocation.getCurrentPosition(showPosition);
+    
+    var versiuneBrowser = document.getElementById("versiuneBrowser");
+    versiuneBrowser.innerHTML = window.navigator.appName + " , Version: " + window.navigator.appVersion;
+
+    var so = document.getElementById("so");
+    let userAgent = window.navigator.userAgent;
+    let os = "Unknown OS";
+
+    if (userAgent.indexOf("Win") != -1) os = "Windows";
+    if (userAgent.indexOf("Mac") != -1) os = "MacOS";
+    if (userAgent.indexOf("X11") != -1) os = "UNIX";
+    if (userAgent.indexOf("Linux") != -1) os = "Linux";
+    so.innerHTML = os;
 }
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude; 
+  }
+
