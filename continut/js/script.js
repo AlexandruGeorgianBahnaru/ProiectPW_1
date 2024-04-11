385b5015068aeb85421031cc4430fd5eaabe0e0e
AlexandruGeorgianBahnaru <130040776+AlexandruGeorgianBahnaru@users.noreply.github.com>
2024-04-11 10:26:14 +0300

    suplimentar acasa

--- continut/js/script.js
+++ continut/js/script.js
@@ -24,9 +24,48 @@ window.onload = function dateTimeFunction()
     if (userAgent.indexOf("X11") != -1) os = "UNIX";
     if (userAgent.indexOf("Linux") != -1) os = "Linux";
     so.innerHTML = os;
+
+    document.registrationForm.age.oninput = function(){
+        document.registrationForm.labelAge.value = document.registrationForm.age.value;
+     }
+    
 }
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude; 
+}
+
+function updateRangeinput(val) {
+    document.getElementById('labelAge').value=val; 
   }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    const currentPage = window.location.pathname;
+    const desenPage = "desen.html";
+    const desprePage = "despre.html";
+    const videoPage = "video.html";
+    const inregistreazaPage = "inregistreaza.html";
+    const invatPage = "invat.html";
+    if (currentPage.endsWith(desenPage)) {
+      // If the associated page is loaded, change the button style
+      document.getElementById('buttonDesen').classList.add('current-page-button');
+    }
+    else if (currentPage.endsWith(videoPage)) {
+        // If the associated page is loaded, change the button style
+        document.getElementById('buttonVideo').classList.add('current-page-button');
+    }
+    else if (currentPage.endsWith(desprePage)) {
+        // If the associated page is loaded, change the button style
+        document.getElementById('buttonDespre').classList.add('current-page-button');
+    }
+    else if (currentPage.endsWith(invatPage)) {
+        // If the associated page is loaded, change the button style
+        document.getElementById('buttonInvat').classList.add('current-page-button');
+    }
+    else if (currentPage.endsWith(inregistreazaPage)) {
+        // If the associated page is loaded, change the button style
+        document.getElementById('buttonInregistreaza').classList.add('current-page-button');
+    }
+  });
 
--- server_web/server_web.py
+++ server_web/server_web.py
@@ -1,46 +1,85 @@
 import socket
-# creeaza un server socket
+import os
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# specifica ca serverul va rula pe portul 5678, accesibil de pe orice ip al serverului
 serversocket.bind(('', 5678))
-# serverul poate accepta conexiuni; specifica cati clienti pot astepta la coada
 serversocket.listen(5)
 
 while True:
- print ('#########################################################################')
- print ('Serverul asculta potentiali clienti.')
- # asteapta conectarea unui client la server
- # metoda `accept` este blocanta => clientsocket, care reprezinta socket-ul corespunzator clientului conectat
- (clientsocket, address) = serversocket.accept()
- print ('S-a conectat un client.')
- # se proceseaza cererea si se citeste prima linie de text
- cerere = ''
- linieDeStart = ''
- while True:
-    data = clientsocket.recv(1024)
-    cerere = cerere + data.decode()
-    print ('S-a citit mesajul: \n---------------------------\n' + cerere + '\n---------------------------')
-    pozitie = cerere.find('\r\n')
-    if (pozitie > -1):
-        linieDeStart = cerere[0:pozitie]
-        print ('S-a citit linia de start din cerere: ##### ' + linieDeStart + '#####')
-        break
-    print ('S-a terminat cititrea.')
- # TODO interpretarea sirului de caractere `linieDeStart` pentru a extrage numele resursei cerute
-    stringSplited = linieDeStart.split('/')
-    stringSplited2 = stringSplited[1].split(' ')
- # TODO trimiterea răspunsului HTTP
-    #response_proto = 'HTTP/1.1'
-    #response_status = '200'
-    #response_status_text = stringSplited[0] # this can be random
+    print('#########################################################################')
+    print('Serverul asculta potentiali clienti.')
+    
+    (clientsocket, address) = serversocket.accept()
+    print('S-a conectat un client.')
+    
+    cerere = ''
+    linieDeStart = ''
+    
+    while True:
+        data = clientsocket.recv(1024)
+        cerere = cerere + data.decode()
+        print('S-a citit mesajul: \n---------------------------\n' + cerere + '\n---------------------------')
+        pozitie = cerere.find('\r\n')
         
-        # sending all this stuff
-    #clientsocket.send('%s %s %s' % (response_proto, response_status, response_status_text))
-    html_file  = "D:\Facultate\PW\proiect-1-AlexandruGeorgianBahnaru1\continut" + "\\" + stringSplited2[0]
-    response = ''
-    t=open(html_file, "r")
-    for v in t.readlines():
-        response.append(v + "\n")
-    clientsocket.sendall('%s' % (response))
- clientsocket.close()
- print ('S-a terminat comunicarea cu clientul.')
+        if pozitie > -1:
+            linieDeStart = cerere[0:pozitie]
+            print('S-a citit linia de start din cerere: ##### ' + linieDeStart + '#####')
+            break
+        print('S-a terminat cititrea.')
+        
+    if linieDeStart == '':
+        clientsocket.close()
+        print('S-a terminat comunicarea cu clientul - nu s-a primit niciun mesaj.')
+        continue
+    
+    elementeLineDeStart = linieDeStart.split()
+    numeResursaCeruta = elementeLineDeStart[1]
+    
+    if numeResursaCeruta == '/':
+        numeResursaCeruta = '/index.html'
+
+    numeFisier = 'D:\\Facultate\\PW\\proiect-1-AlexandruGeorgianBahnaru1\\continut' + numeResursaCeruta
+    print(numeFisier)
+    
+    fisier = None
+    try:
+        fisier = open(numeFisier, 'rb')
+        numeExtensie = numeFisier[numeFisier.rfind('.') + 1:]
+        tipuriMedia = {
+            'html': 'text/html; charset=utf-8',
+            'css': 'text/css; charset=utf-8',
+            'js': 'text/javascript; charset=utf-8',
+            'png': 'image/png',
+            'jpg': 'image/jpeg',
+            'jpeg': 'image/jpeg',
+            'gif': 'image/gif',
+            'ico': 'image/x-icon',
+            'xml': 'application/xml; charset=utf-8',
+            'json': 'application/json; charset=utf-8'
+        }
+        tipMedia = tipuriMedia.get(numeExtensie, 'text/plain; charset=utf-8')
+
+        clientsocket.sendall('HTTP/1.1 200 OK\r\n'.encode())
+        clientsocket.sendall(('Content-Length: ' + str(os.stat(numeFisier).st_size) + '\r\n').encode())
+        clientsocket.sendall(('Content-Type: ' + tipMedia + '\r\n').encode())
+        clientsocket.sendall('Server: guitar\r\n'.encode())
+        clientsocket.sendall('\r\n'.encode())
+
+        buf = fisier.read(1024)
+        while buf:
+            clientsocket.send(buf)
+            buf = fisier.read(1024)
+    except IOError:
+        msg = 'Eroare! Resursa ceruta ' + numeResursaCeruta + ' nu a putut fi gasita!'
+        print(msg)
+        clientsocket.sendall('HTTP/1.1 404 Not Found\r\n'.encode())
+        clientsocket.sendall(('Content-Length: ' + str(len(msg.encode('utf-8'))) + '\r\n').encode())
+        clientsocket.sendall('Content-Type: text/plain; charset=utf-8\r\n'.encode())
+        clientsocket.sendall('Server: My PW Server\r\n'.encode())
+        clientsocket.sendall('\r\n'.encode())
+        clientsocket.sendall(msg.encode())
+    finally:
+        if fisier is not None:
+            fisier.close()
+    clientsocket.close()
+    print('S-a terminat comunicarea cu clientul.')
